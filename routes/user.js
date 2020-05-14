6bb4dde7838c0a8956b4d55e9501660af4cc4dc1
arunmopare <arunmopare546@gmail.com>
2020-05-14 19:25:16 +0530

    added pushOrderInPurchaseList

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const router = express.Router()
 
-const { getUserById, getUser } = require("../controllers/user")
-const { isSignedin,isAuthenticated,isAdmin } = require("../controllers/auth")
+const { getUserById, getUser, updateUser, userPurchaseList } = require("../controllers/user")
+const { isSignedin, isAuthenticated, isAdmin } = require("../controllers/auth")
 
 router.param("userID", getUserById);
 
-router.get("/user/:userID", isSignedin,isAuthenticated,getUser);
+router.get("/user/:userID", isSignedin, isAuthenticated, getUser);
+router.put("/user/:userID", isSignedin, isAuthenticated, updateUser);
 
+router.get("/orders/user/:userID", isSignedin, isAuthenticated, userPurchaseList);
 
 module.exports = router;
